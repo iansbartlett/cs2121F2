8d4df2211efcf0e7f89b9bd8fc8cbe98c2266d64
iansbartlett <iansbartlett@gmail.com>
2016-05-23 17:14:51 +1000

    Tinkered with PWM- can get PWM output on OC3A on arduino but not board, cannot get OC3B at all. Confused.

--- Labs/Lab_05/Lab5_B.asm
+++ Labs/Lab_05/Lab5_B.asm
@@ -1,21 +1,24 @@
-//Lab 5 Part C
+//Lab 5 Part B	
 //Ian Bartlett z3419581 and Aaron Schneider z502001
 
 .include "m2560def.inc"
 
-ldi r16, 0b00000100
+.cseg
 
-out DDRE, r16; Bit 3 will function as OC5A.
+//ldi r16, 0b00010000
+
+ser r16
+out DDRE, r16; Bit 3 will function as OC3.
 ldi r16, 0x4A; the value controls the PWM duty cycle
-out OCR3BL, r16
+sts OCR3BL, r16
 clr r16
-out OCR3BH, r16
+sts OCR3BH, r16
 
-; Set the Timer3 to Phase Correct PWM mode. 
-ldi temp, (1 << CS50)
-out TCCR3B, r16
-ldi r16, (1<< WGM50)|(1<<COM5A1)
-out TCCR3A, r16
+//Attempting to use Fast PWM on Port E pin 3
+ldi r16, (1 << CS30)
+sts TCCR3B, r16
+ldi r16, (1 << WGM30)|(1 << WGM32 )|(1<<COM3A1)
+sts TCCR3A, r16
 
 halt:
    rjmp halt
